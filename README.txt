bed341ad1ba2f72bda9ca83d3b59bfb7e029bc7e
Ashraf Khan <akuideveloper@gmail.com>
2025-08-15 19:15:40 +0530

    Initial

---
+++ README.txt
@@ -0,0 +1,45 @@
+=== Dummy Post Generator ===
+Contributors: yourname
+Tags: posts, dummy, test, generator, custom post type
+Requires at least: 5.0
+Tested up to: 6.5
+Requires PHP: 7.4
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+Easily generate dummy posts or custom post types for testing and development purposes. Supports bulk creation and works with any public post type.
+
+== Installation ==
+1. Upload the plugin files to the `/wp-content/plugins/dummy-post-generator` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+3. Navigate to 'Dummy Posts' in the WordPress admin menu.
+
+== Usage ==
+1. Go to 'Dummy Posts' in your WordPress admin menu.
+2. Select the number of posts to generate (1-100).
+3. Choose the post type (Post, Page, or any custom post type).
+4. Click 'Generate Dummy Posts'.
+5. Success or error messages will be displayed after generation.
+
+== Frequently Asked Questions ==
+= Can I generate custom post types? =
+Yes, any public custom post type registered in your WordPress site will be available in the dropdown.
+
+= Is there a limit to the number of posts I can generate? =
+You can generate between 1 and 100 posts per action.
+
+= Does this plugin delete posts? =
+No, it only creates new posts. You must delete unwanted posts manually.
+
+== Changelog ==
+= 1.1 =
+* Added internationalization and escaping for all user-facing strings.
+* Improved security and code quality.
+
+= 1.0 =
+* Initial release.
+
+== License ==
+This plugin is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
---
+++ dummy-post-generator.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Plugin Name: Dummy Post Generator
+ * Description: Generate dummy posts for testing purposes, including custom post types
+ * Version: 1.1
+ * Author: Ashraf Khan
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Add admin menu
+function dpg_add_admin_menu() {
+    add_menu_page(
+        __( 'Dummy Post Generator', 'dummy-post-generator' ),
+        __( 'Dummy Posts', 'dummy-post-generator' ),
+        'manage_options',
+        'dummy-post-generator',
+        'dpg_admin_page',
+        'dashicons-text-page',
+        30
+    );
+}
+add_action('admin_menu', 'dpg_add_admin_menu');
+
+// Get available post types
+function dpg_get_post_types() {
+    $args = array(
+        'public'   => true,
+        '_builtin' => false
+    );
+    $custom_types = get_post_types($args, 'objects');
+    $post_types = array('post' => 'Post', 'page' => 'Page');
+    
+    foreach ($custom_types as $type) {
+        $post_types[$type->name] = $type->label;
+    }
+    
+    return $post_types;
+}
+
+// Admin page content
+function dpg_admin_page() {
+    $post_types = dpg_get_post_types();
+    ?>
+    <div class="wrap">
+        <h1><?php esc_html_e( 'Dummy Post Generator', 'dummy-post-generator' ); ?></h1>
+        <form method="post" action="">
+            <?php wp_nonce_field( 'dpg_generate_posts', 'dpg_nonce' ); ?>
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="post_count"><?php esc_html_e( 'Number of Posts', 'dummy-post-generator' ); ?></label></th>
+                    <td>
+                        <input type="number" id="post_count" name="post_count" min="1" max="100" value="10" required>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="post_type"><?php esc_html_e( 'Post Type', 'dummy-post-generator' ); ?></label></th>
+                    <td>
+                        <select id="post_type" name="post_type">
+                            <?php foreach ( $post_types as $type => $label ) : ?>
+                                <option value="<?php echo esc_attr( $type ); ?>"><?php echo esc_html( $label ); ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button( __( 'Generate Dummy Posts', 'dummy-post-generator' ) ); ?>
+        </form>
+    </div>
+    <?php
+}
+
+// Generate dummy posts
+function dpg_generate_dummy_posts($count, $post_type) {
+    $dummy_content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+    for ($i = 1; $i <= $count; $i++) {
+        $post_title = "Dummy " . ucfirst($post_type) . " " . $i;
+        $post_content = $dummy_content;
+
+        $post_id = wp_insert_post(array(
+            'post_title'    => $post_title,
+            'post_content'  => $post_content,
+            'post_status'   => 'publish',
+            'post_author'   => get_current_user_id(),
+            'post_type'     => $post_type
+        ));
+
+        if (is_wp_error($post_id)) {
+            error_log('Error creating dummy post: ' . $post_id->get_error_message());
+        }
+    }
+}
+
+// Handle form submission
+function dpg_handle_form_submission() {
+    if (isset($_POST['dpg_nonce']) && wp_verify_nonce($_POST['dpg_nonce'], 'dpg_generate_posts')) {
+        if (isset($_POST['post_count']) && is_numeric($_POST['post_count']) && isset($_POST['post_type'])) {
+            $post_count = intval($_POST['post_count']);
+            $post_type = sanitize_text_field($_POST['post_type']);
+            
+            if ($post_count > 0 && $post_count <= 100 && post_type_exists($post_type)) {
+                dpg_generate_dummy_posts($post_count, $post_type);
+                add_action('admin_notices', 'dpg_admin_notice_success');
+            } else {
+                add_action('admin_notices', 'dpg_admin_notice_error');
+            }
+        }
+    }
+}
+add_action('admin_init', 'dpg_handle_form_submission');
+
+// Success notice
+function dpg_admin_notice_success() {
+    ?>
+    <div class="notice notice-success is-dismissible">
+        <p><?php esc_html_e( 'Dummy posts generated successfully!', 'dummy-post-generator' ); ?></p>
+    </div>
+    <?php
+}
+
+// Error notice
+function dpg_admin_notice_error() {
+    ?>
+    <div class="notice notice-error is-dismissible">
+        <p><?php esc_html_e( 'Please enter a valid number of posts (between 1 and 100) and select a valid post type.', 'dummy-post-generator' ); ?></p>
+    </div>
+    <?php
+}
